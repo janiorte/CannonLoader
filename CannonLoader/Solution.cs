36787f868619c97f3e885f218f88cf8322f02553
Joaquín Aniorte <dreammaker@gmail.com>
2021-02-22 20:15:28 +0100

    Initial commit

---
+++ CannonLoader/Solution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CannonLoader
+{
+    public class CannonLoaderSolution
+    {
+        private IEnumerable<int> PickIndexes(int[] A)
+        {
+            for (var i = 1; i < A.Length - 1; i++)
+            {
+                if (A[i] > A[i - 1] && A[i] > A[i + 1])
+                    yield return i;
+            }
+        }
+
+        private bool CanGetLargerDistances(int[] indexes, int distance)
+        {
+            var totalDistances = 1;
+            var currentDistance = 0;
+            for (var i = 0; i < indexes.Length - 1; i++)
+            {
+                currentDistance += indexes[i + 1] - indexes[i];
+                if (currentDistance >= distance)
+                {
+                    totalDistances++;
+                    currentDistance = 0;
+                    if (totalDistances == distance)
+                        return true;
+                }
+            }
+
+            return totalDistances >= distance;
+        }
+
+        public int Solution(int[] A)
+        {
+            if (A == null || A.Length == 0)
+                throw new ArgumentException("Picks array not valid");
+
+            var picks = PickIndexes(A).ToArray();
+            if (picks.Length == 0)
+                throw new ArgumentException("No picks found");
+
+            // Minimum iterations cannons number guess
+            int min = 0, max = picks.Length + 1;
+            while (min + 1 < max)
+            {
+                var guess = (min + max) / 2;
+                if (CanGetLargerDistances(picks, guess))
+                {
+                    min = guess;
+                }
+                else
+                {
+                    max = guess;
+                }
+            }
+
+            return min;
+        }
+    }
+}
---
+++ CannonLoaderTests/SolutionTests.cs
@@ -0,0 +1,62 @@
+﻿using CannonLoader;
+using NUnit.Framework;
+using System;
+
+namespace CannonLoaderTests
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        private CannonLoaderSolution sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            sut = new CannonLoaderSolution();
+        }
+
+        [Test]
+        public void Example_Test()
+        {
+            int solution = sut.Solution(new int[] { 1, 6, 4, 5, 4, 5, 1, 2, 3, 4, 7, 2 });
+            Assert.AreEqual(3, solution);
+        }
+
+        [Test]
+        public void Example2_Test()
+        {
+            int solution = sut.Solution(new int[] { 1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 });
+            Assert.AreEqual(3, solution);
+        }
+
+        [Test]
+        public void NullInput_Test()
+        {
+            Assert.Throws<ArgumentException>(() => sut.Solution(null));
+        }
+
+        [Test]
+        public void VoidInputArray_Test()
+        {
+            Assert.Throws<ArgumentException>(() => sut.Solution(new int[] { }));
+        }
+
+        [Test]
+        public void NoPicksArray_Test()
+        {
+            Assert.Throws<ArgumentException>(() => sut.Solution(new int[] { 1, 1, 1, 1 }));
+        }
+
+        [TestCase(new[] { 1, 2, 1 }, ExpectedResult = 1)]
+        [TestCase(new[] { 1, 3, 1, 2, 1 }, ExpectedResult = 2)]
+        [TestCase(new[] { 1, 4, 1, 1, 2, 1 }, ExpectedResult = 2)]
+        [TestCase(new[] { 1, 5, 1, 1, 2, 1, 2, 1 }, ExpectedResult = 2)]
+        [TestCase(new[] { 1, 6, 1, 1, 2, 1, 1, 2, 1 }, ExpectedResult = 3)]
+        [TestCase(new[] { 1, 7, 1, 1, 2, 1, 2, 1, 1, 2, 1 }, ExpectedResult = 3)]
+        [TestCase(new[] { 1, 1, 1, 1, 1, 8, 1, 1, 2, 1, 2, 1, 1, 2, 1 }, ExpectedResult = 3)]
+        public int ValidArray_Tests(int[] picks)
+        {
+            return sut.Solution(picks);
+        }
+    }
+}
